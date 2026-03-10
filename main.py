aa5c69eac728165e9c175fdfd60349bc99061c47
Jevgenijs93 <jevgenijs.sinkevics93@gmail.com>
2026-03-10 20:04:14 +0200

    third commit

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-age = int(input("Enter your age: "))
-if age >=18:
-    print("You are adult user.")
-else:
-    print("You are not adult user.")
+tasks = []
+
+while True:
+    print("Menu:")
+    print("1. Show tasks:")
+    print("2. Add task:")
+    print("3. Exit")
+
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        if len(tasks) == 0:
+            print("Task list is empty")
+        else:
+            print("Task list:")
+            for index, task in enumerate(tasks, start=1):
+                print(index, "-", task)
